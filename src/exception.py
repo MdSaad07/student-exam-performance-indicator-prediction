4a55dd1bf57a90a43a6a0172821b7c09a1af3ce2
MdSaad07 <mdsaad7803@gmail.com>
2024-07-22 01:33:13 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,29 @@
+## this file is for handling exception
+
+import sys
+import logging
+from logger import logger  # import the logger from the logger.py file in which it is exported
+
+def error_message_details(error, error_detail:sys):
+    """This function generates custom error messages."""
+    _, _, exc_tb = error_detail.exc_info()  ##provide info about the exception that is encountered
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail = error_detail)
+        
+    def __str__(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e, sys)
---
+++ src/utils.py
@@ -0,0 +1,1 @@
+## this file is used for writing function that can be used anywhere
