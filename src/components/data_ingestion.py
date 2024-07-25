abcca2aca9986e2c6a7e0d54fdf7d8a3200909f6
MdSaad07 <mdsaad7803@gmail.com>
2024-07-25 14:30:01 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,13 +7,17 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass
-class DataIngestionConfig:
+@dataclass #dataclass is used to define simple classes for holding configurations.
+class DataIngestionConfig: #holds the file paths for the raw data, training data, and testing data.
     train_data_path: str = os.path.join('artifacts/', "train.csv") #use / after artifacts to ignore the formation of folder
     test_data_path: str = os.path.join("artifacts/", "test.csv")
     raw_data_path: str = os.path.join("artifacts/", "data.csv")
@@ -51,4 +55,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,17 @@
-## this file is used for writing function that can be used anywhere
+## this file is used for writing commonly used functions that can be used anywhere
+import os
+import sys
+import dill
+
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
